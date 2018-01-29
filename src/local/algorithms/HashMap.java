bf0b66ceb41165d39325bd094401f24768bcf35c
rafaelsfrr <rafael.sobfer@gmail.com>
2018-01-29 13:53:40 -0200

    Implemented HashMap

--- src/local/algorithms/HashMap.java
+++ src/local/algorithms/HashMap.java
@@ -1,140 +1,120 @@
 package local.algorithms;
 
+import java.lang.reflect.Array;
+
 public class HashMap<K,V> {
-    private ArrayList<NodeMap<K,V>> map = new ArrayList<>();
+    private NodeMap<K,V>[] map;
+
+    public HashMap(Class<NodeMap> clazz, int size) {
+        map = (NodeMap<K,V>[]) Array.newInstance(clazz, size);
+    }
+
+    public NodeMap[] getMap() {
+        return map;
+    }
 
     // O(1)
-    public int getHash(K key) {
+    public int getHash(K key) throws Exception {
 
         // This method accept a String or a Integer key
         if(key instanceof Integer) {
-            return ((Integer) key).intValue() % map.size();
+            return ((Integer) key).intValue() % map.length;
         } else if(key instanceof String) {
-            return Integer.getInteger((String) key).intValue() % map.size();
+            return Integer.getInteger((String) key).intValue() % map.length;
         } else {
-            return -1;
+            throw new Exception("Key must be a String or a Integer");
         }
     }
 
     // O(1) because we always add on the first element
-    public void add(K key, V value) {
+    public void add(K key, V value) throws Exception {
         int index = getHash(key);
-        // Check if the hash works, i.e., String or Integer key
-        if(index > -1) {
-            NodeMap<K,V> nn = new NodeMap<>();
-            nn.setKey(key);
-            nn.setValue(value);
-            nn.setNext(map.getElement(index));
-            map.add(nn, index);
-        }
+        NodeMap<K,V> nn = new NodeMap<>();
+        nn.setKey(key);
+        nn.setValue(value);
+        nn.setNext(map[index]);
+        map[index] = nn;
     }
 
     // O(1) if no conflicts and O(N) if has conflicts
-    public V getValue(K key) {
+    public V get(K key) throws Exception{
         int index = getHash(key);
 
-        // Check if the hash works, i.e., String or Integer key
-        if(index > -1) {
-            NodeMap<K,V> it;
-            it = map.getElement(index);
-            System.out.println(it.getValue());
+        NodeMap<K,V> it;
+        it = map[index];
 
-            // Search the list for the element. If there is no conflicts with the hash, this
-            // method is O(1), otherwise is O(list.lenght), i.e, O(N)
-            while(it.getKey() != key) {
-                if(it.getNext() != null) {
-                    it = it.getNext();
-                } else {
-                    return null;
-                }
-            }
-            return it.getValue();
-        } else {
-            return null;
-        }
-    }
-
-    public ArrayList<NodeMap<K, V>> getMap() {
-        return map;
-    }
-
-    public void setMap(ArrayList<NodeMap<K, V>> map) {
-        this.map = map;
-    }
-
-    // O(1) if no conflicts and O(N) if has conflicts
-    public void removeValue(K key) {
-        int index = getHash(key);
-
-        // Check if the hash works, i.e., String or Integer key
-        if(index > -1) {
-            NodeMap<K,V> it, aux;
-            it = map.getElement(index);
-            aux = it;
-
-            // Search the list for the element. If there is no conflicts with the hash, this
-            // method is O(1), otherwise is O(list.lenght), i.e, O(N)
-            while(it.getKey() != key) {
-                if(it.getNext() != null) {
-                    aux = it;
-                    it = it.getNext();
-                } else {
-                    aux = null;
-                    it = null;
-                    break;
-                }
-            }
-
-            if(map.getElement(index) == it) {
-                map.add(it.getNext(), index);
+        // Search the list for the element. If there is no conflicts with the hash, this
+        // method is O(1), otherwise is O(list.lenght), i.e, O(N)
+        while(it.getNext() != null) {
+            if(it.getKey() != key) {
+                it = it.getNext();
             } else {
-                aux.setNext(it.getNext());
+                return it.getValue();
             }
+        }
+        return null;
+    }
+
+    // O(1) if no conflicts and O(N) if has conflicts
+    public void remove(K key) throws Exception {
+        int index = getHash(key);
+
+        NodeMap<K,V> it, aux;
+        it = map[index];
+        aux = it;
+
+        // Search the list for the element. If there is no conflicts with the hash, this
+        // method is O(1), otherwise is O(list.lenght), i.e, O(N)
+        while(it.getNext() != null) {
+            if(it.getKey() != key) {
+                aux = it;
+                it = it.getNext();
+            } else {
+                break;
+            }
+        }
+
+        if(map[index] == it) {
+            map[index] = it.getNext();
+        } else {
+            aux.setNext(it.getNext());
         }
 
 
     }
 
     public static void main(String[] args) {
-        HashMap<Integer,Integer> hash = new HashMap<>();
+        HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>(
+                NodeMap.class,10);
 
-        hash.add(Integer.valueOf(1),Integer.valueOf(10));
-        hash.add(Integer.valueOf(5),Integer.valueOf(20));
-        hash.add(Integer.valueOf(2),Integer.valueOf(250));
-        hash.add(Integer.valueOf(10),Integer.valueOf(11));
-        hash.add(Integer.valueOf(30),Integer.valueOf(91));
+        try {
+            hash.add(Integer.valueOf(1),Integer.valueOf(10));
+            hash.add(Integer.valueOf(5),Integer.valueOf(20));
+            hash.add(Integer.valueOf(2),Integer.valueOf(250));
+            hash.add(Integer.valueOf(10),Integer.valueOf(11));
+            hash.add(Integer.valueOf(30),Integer.valueOf(91));
 
-        // <1,10>
-        System.out.println("Items<K,V>: ");
-        System.out.println("<" +
-                "1" +
-                "," +
-                hash.getValue(1) + ">");
+            System.out.println("Items <K,V>: ");
+            for(int i = 0; i < hash.getMap().length; i++) {
+                if(hash.getMap()[i] != null) {
+                    NodeMap<Integer,Integer> it = hash.getMap()[i];
 
-        // <5,20>
-        System.out.println("<" +
-                "5" +
-                "," +
-                hash.getValue(5) + ">");
+                    System.out.println("<" + it.getKey() + "," +
+                            it.getValue() + ">");
+                    while(it.getNext() != null) {
+                        it = it.getNext();
+                        System.out.println("    <" + it.getKey() + "," +
+                                it.getValue() + ">");
+                    }
 
-        // <2,250>
-        System.out.println("<" +
-                "2" +
-                "," +
-                hash.getValue(2) + ">");
+                } else {
+                    System.out.println("null");
+                }
+            }
 
-        // <10,11>
-        System.out.println("<" +
-                "10" +
-                "," +
-                hash.getValue(10) + ">");
-
-        // <30,91>
-        System.out.println("<" +
-                "30" +
-                "," +
-                hash.getValue(30) + ">");
-
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
