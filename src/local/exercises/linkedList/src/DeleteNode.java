fef769b8ffded8c9f892c55b6d1db317906967eb
rafaelsfrr <rafael.sobfer@gmail.com>
2018-02-11 13:44:14 -0200

    Solved the exercise of delete a given node from a linked list

--- src/local/exercises/linkedList/src/DeleteNode.java
+++ src/local/exercises/linkedList/src/DeleteNode.java
@@ -1,4 +1,35 @@
 package local.exercises.linkedList.src;
 
+import local.algorithms.LinkedList;
+import local.algorithms.Node;
+
+// O(1) time and space
 public class DeleteNode {
+
+    public static void delete(Node node) {
+        if(node != null && node.getNext() != null) {
+            node.setElement(node.getNext().getElement());
+            node.setNext(node.getNext().getNext());
+        }
+    }
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(Integer.valueOf(1));
+        list.add(Integer.valueOf(4));
+        list.add(Integer.valueOf(1));
+        list.add(Integer.valueOf(12));
+        list.add(Integer.valueOf(12));
+        list.add(Integer.valueOf(16));
+
+        DeleteNode.delete(list.getNodeByIndex(3));
+        Node<Integer> node = list.getFirstNode();
+
+        while(node != null) {
+            System.out.println(node.getElement());
+            node = node.getNext();
+        }
+
+    }
 }
