92b03e6257ef9ac067a2e573e5148c60769d3110
rafaelsfrr <rafael.sobfer@gmail.com>
2018-02-23 15:20:11 -0300

    Added EmptyStackException

--- src/local/exercises/stackQueue/QueueOfStacks.java
+++ src/local/exercises/stackQueue/QueueOfStacks.java
@@ -1,4 +1,53 @@
 package local.exercises.stackQueue;
 
-public class QueueOfStacks {
+import java.util.EmptyStackException;
+import java.util.Stack;
+
+// We use two stacks to implement a queue, one being the reverse of the other
+public class QueueOfStacks<E> {
+    // O(A+B) space
+    private Stack<E> reverseStack;
+    private Stack<E> normalStack;
+
+    public QueueOfStacks() {
+        reverseStack = new Stack<>();
+        normalStack = new Stack<>();
+    }
+
+    // O(1)
+    public int size() {
+        return normalStack.size() + reverseStack.size();
+    }
+
+    // We use a lazy evaluation, sending data to the reverseStack just when this is empty
+    // O(N) ~ O(1)
+    public E peek() {
+        if(reverseStack.isEmpty())
+            sendToReverse();
+
+        if(!reverseStack.isEmpty())
+            return reverseStack.peek();
+        else throw new EmptyStackException();
+    }
+
+    // O(N)
+    public void sendToReverse() {
+        while(!normalStack.isEmpty())
+            reverseStack.push(normalStack.pop());
+    }
+
+    // O(1)
+    public void add(E data) {
+        normalStack.push(data);
+    }
+
+    // O(N) ~ O(1)
+    public E remove() {
+        if(reverseStack.isEmpty())
+            sendToReverse();
+
+        if(!reverseStack.isEmpty())
+            return reverseStack.pop();
+        else throw new EmptyStackException();
+    }
 }
